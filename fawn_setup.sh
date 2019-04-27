7c0acbab7ef2556b1e65e63795ba32ddc4d8ca72
Anirban Biswas <anirban.biswas07@gmail.com>
2019-04-27 03:53:20 -0700

    added openssl and upgrading dependencies after installing

---
+++ fawn_setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+FAWN_HOME="${HOME}/FAWN-KV"
+INSTALLATION_DIR="${HOME}"
+
+echo "Installing required packages..."
+apt install -y \
+    ant \
+    autoconf \
+    automake \
+    autotools-dev \
+    bison \
+    build-essential \
+    cmake \
+    flex \
+    g++ \
+    libboost-all-dev \
+    libboost-dev \
+    libboost-filesystem-dev \
+    libboost-program-options-dev \
+    libboost-test-dev \
+    libboost-thread-dev \
+    libevent-dev \
+    libgtest-dev \
+    libssl-dev \
+    libtbb-dev \
+    libtbb2 \
+    libtool \
+    m4 \
+    openssl \
+    patch \
+    pkg-config \
+    python-dev \
+    ruby-dev \
+    software-properties-common \
+    tar \
+    vim \
+    wget
+apt update
+apt upgrade
+echo "Done"
+
+echo "Installing php..."
+locale-gen en_US.UTF-8
+LANG=en_US.UTF-8
+
+add-apt-repository ppa:ondrej/php -y
+apt-get update
+
+apt install -y php5.6-dev
+echo "Done"
+
+echo "Installing thrift..."
+tar -xjf ${FAWN_HOME}/setup/thrift-0.5.0.tar.bz2 -C ${INSTALLATION_DIR}
+cd ${INSTALLATION_DIR}/thrift-0.5.0
+patch -p1 < ${FAWN_HOME}/patches/fawn-thrift.patches
+./configure
+make
+make install
+cd -
+echo "Done"
+
+echo "Installing gtest..."
+tar -xzf ${FAWN_HOME}/setup/googletest-release-1.7.0.tar.gz -C ${INSTALLATION_DIR}
+cd ${INSTALLATION_DIR}/googletest-release-1.7.0
+cmake -DBUILD_SHARED_LIBS=ON .
+make
+cp -a include/gtest /usr/include/
+cp -a libgtest_main.so libgtest.so /usr/lib/
+cd -
+echo "Done"
+
+echo "Installing FAWN..."
+cd ${FAWN_HOME}
+autoreconf -is
+./configure
+make
+cd -
+echo "Done"
+
+
+cd $HOME
